830376df5fa55c1ecc43860cb02f0a88beb37ffc
jeetiitd <jeeta@griphic.com>
2026-05-26 16:09:01 +0530

    Implement user authentication with JWT and add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
 const router=Router()
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 router.route("/register").post(
     upload.fields([
@@ -16,5 +18,13 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 
 export default router
